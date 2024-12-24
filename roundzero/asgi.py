e9c82dee892ac626594e6324b4ad1c56dae83283
tibssy <norbert.tiborcz@gmail.com>
2024-12-24 01:16:40 +0000

    integrate the channels library

--- roundzero/asgi.py
+++ roundzero/asgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roundzero.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        # Just HTTP for now. (We can add other protocols later.)
+    }
+)

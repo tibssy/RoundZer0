3250088ad537ce8aa487d235dc33817a78a609a4
tibssy <norbert.tiborcz@gmail.com>
2024-12-27 01:07:34 +0000

    Refactor VoiceConsumer class

--- chatbot/consumers.py
+++ chatbot/consumers.py
@@ -1,14 +1,16 @@
+import os
+import io
+import json
+import re
 from channels.generic.websocket import AsyncWebsocketConsumer
 from openai import OpenAI
 import edge_tts
-import re
-import json
-import os
-import io
+
+# Load environment variables if env.py exists
 if os.path.isfile('env.py'):
     import env
 
-
+# Constants
 LANGUAGE = 'en'
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 SYSTEM_MESSAGE = "You are a professional interview assistant. Respond with clear and professional questions and answers"
@@ -16,33 +18,36 @@ SYSTEM_MESSAGE = "You are a professional interview assistant. Respond with clear
 
 class VoiceConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Initialize an Assistant instance for this WebSocket connection
+        """Initialize an Assistant instance for this WebSocket connection."""
         self.assistant = Assistant(api_key=OPENAI_API_KEY, language=LANGUAGE)
         await self.accept()
 
     async def disconnect(self, close_code):
+        """Handle disconnection."""
         pass
 
     async def receive(self, text_data=None, bytes_data=None):
-
+        """Process received data from WebSocket."""
         if bytes_data:
-            # Process the voice data received from WebRTC
-            audio_buffer = io.BytesIO(bytes_data)
-            audio_buffer.name = 'speech.ogg'
-            audio_buffer.seek(0)
+            await self.process_voice_data(bytes_data)
 
-            text = self.assistant.speech_to_text(audio_buffer)
-            print(f"Received voice data: {len(bytes_data)} bytes.\n\nText: {text}")
+        await self.send(text_data=json.dumps({"message": "Data received"}))
 
+    async def process_voice_data(self, bytes_data):
+        """Process the voice data received from WebRTC."""
+        audio_buffer = io.BytesIO(bytes_data)
+        audio_buffer.name = 'speech.ogg'
+        audio_buffer.seek(0)
+
+        # text = self.assistant.speech_to_text(audio_buffer)
+        if text := self.assistant.speech_to_text(audio_buffer).strip():
             text_stream = self.assistant.openai_chat(text)
             sentences = self.assistant.stream_to_sentences(text_stream)
+
             for sentence in sentences:
-                print(sentence)
                 audio_data = await self.assistant.text_to_speech(sentence)
                 if audio_data:
                     await self.send(bytes_data=audio_data)
-
-        await self.send(text_data=json.dumps({"message": "Data received"}))
 
 
 class Assistant:

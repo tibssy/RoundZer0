7f0d388cbf06faf9c4389854d68b75ade78feb46
tibssy <norbert.tiborcz@gmail.com>
2024-12-27 23:38:07 +0000

    Create superuser and set secret key

--- roundzero/settings.py
+++ roundzero/settings.py
@@ -24,7 +24,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6pg)+j1%r5mqhsjfc)ekhats-a9z)l@=h!z4!2!o=-$muhhlpu"
+# SECRET_KEY = "django-insecure-6pg)+j1%r5mqhsjfc)ekhats-a9z)l@=h!z4!2!o=-$muhhlpu"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

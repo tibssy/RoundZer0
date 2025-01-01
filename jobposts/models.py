ef7eb4bb84b9b6d3e4a1057bdbe5e4c43000710f
tibssy <norbert.tiborcz@gmail.com>
2025-01-01 02:21:46 +0000

    Add JobPost model

--- jobposts/models.py
+++ jobposts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class JobPost(models.Model):
+    EMPLOYMENT_TYPE_CHOICES = [
+        ('FT', 'Full-time'),
+        ('PT', 'Part-time'),
+        ('CT', 'Contract'),
+        ('IN', 'Internship'),
+    ]
+
+    title = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    employment_type = models.CharField(max_length=2, choices=EMPLOYMENT_TYPE_CHOICES)
+    description = models.TextField()
+    requirements = models.TextField()
+    posted_date = models.DateField(auto_now_add=True)
+    application_deadline = models.DateField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='job_posts')
+
+    def __str__(self):
+        return f"{self.title} at {self.company_name} ({self.location})"

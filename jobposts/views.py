5c024dc2b4feb81d9216742cafd174eacd883db8
tibssy <norbert.tiborcz@gmail.com>
2025-01-02 21:36:31 +0000

    Split fields content like Responsibilities, Requirements, Benefits

--- jobposts/views.py
+++ jobposts/views.py
@@ -4,7 +4,7 @@ from .models import JobPost
 
 
 class JobList(generic.ListView):
-    queryset = JobPost.objects.all().order_by("created_on")
+    queryset = JobPost.objects.all().order_by("-created_on")  # Updated ordering to match model Meta
     template_name = "jobposts/index.html"
     paginate_by = 6
 
@@ -12,3 +12,11 @@ class JobList(generic.ListView):
 class JobDetailView(generic.DetailView):
     model = JobPost
     template_name = 'jobposts/job_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        job_post = self.get_object()
+        context['responsibilities_list'] = [f'{item.strip()}.' for item in job_post.responsibilities.split('.') if item.strip()]
+        context['requirements_list'] = [f'{item.strip()}.' for item in job_post.requirements.split('.') if item.strip()]
+        context['benefits_list'] = [f'{item.strip()}.' for item in job_post.benefits.replace(',', '.').split('.') if item.strip()]
+        return context

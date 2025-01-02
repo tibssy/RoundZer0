a350d49e5f4c43fd425e199cf0bfebba415481fc
tibssy <norbert.tiborcz@gmail.com>
2025-01-02 16:46:06 +0000

    Add job_detail page to view

--- jobposts/urls.py
+++ jobposts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    # path('', views.jobs_index, name='jobs'),
     path('', views.JobList.as_view(), name='jobs'),
+    path('<int:pk>/', views.JobDetailView.as_view(), name='job_detail'),
 ]
--- jobposts/views.py
+++ jobposts/views.py
@@ -7,3 +7,8 @@ class JobList(generic.ListView):
     queryset = JobPost.objects.all().order_by("created_on")
     template_name = "jobposts/index.html"
     paginate_by = 6
+
+
+class JobDetailView(generic.DetailView):
+    model = JobPost
+    template_name = 'jobposts/job_detail.html'

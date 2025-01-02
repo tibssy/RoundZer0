2daff50c066914dd002cdf41b627d4cb26839d87
tibssy <norbert.tiborcz@gmail.com>
2025-01-02 13:35:30 +0000

    Add pagination to job posts

--- jobposts/views.py
+++ jobposts/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from .models import JobPost
 
-# Create your views here.
-# def jobs_index(request):
-#     return render(request, 'jobposts/index.html')
-
 
 class JobList(generic.ListView):
-    # model = JobPost
     queryset = JobPost.objects.all().order_by("created_on")
     template_name = "jobposts/index.html"
+    paginate_by = 6

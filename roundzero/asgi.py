7c959f60a184f0992920240d00996efb780e952f
tibssy <norbert.tiborcz@gmail.com>
2025-01-03 15:41:55 +0000

    Test deployment to heroku: early load DJANGO_SETTINGS_MODULE on asgi

--- roundzero/asgi.py
+++ roundzero/asgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roundzero.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator

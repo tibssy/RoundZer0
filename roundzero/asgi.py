5450f99f9b2249aeab9a28ee9564359b6a9a76c6
tibssy <norbert.tiborcz@gmail.com>
2025-01-03 15:49:36 +0000

    Test deployment to heroku: lazy loading jobposts.models in consumers

--- roundzero/asgi.py
+++ roundzero/asgi.py
@@ -9,8 +9,6 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roundzero.settings")
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -18,7 +16,7 @@ from django.core.asgi import get_asgi_application
 from chatbot.routing import websocket_urlpatterns
 
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roundzero.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "roundzero.settings")
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(

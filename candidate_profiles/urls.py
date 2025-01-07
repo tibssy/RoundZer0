be17d72274a86427bd818e2ed388b8411e63ee80
tibssy <norbert.tiborcz@gmail.com>
2025-01-07 20:58:02 +0000

    Add button to edit profile

--- candidate_profiles/urls.py
+++ candidate_profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('profile/', views.candidate_profile, name='candidate_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('delete_profile/', views.delete_profile_and_account, name='delete_profile_and_account'),
 ]
--- candidate_profiles/views.py
+++ candidate_profiles/views.py
@@ -4,17 +4,21 @@ from django.contrib.auth import logout
 from django.contrib import messages
 from .models import Candidate
 
+
 @login_required
 def candidate_profile(request):
     try:
         candidate = request.user.candidate_profile
     except Candidate.DoesNotExist:
-        # Handle the case where a logged-in user doesn't have a Candidate profile
-        # You might want to redirect them to create a profile or show an error message
-        return render(request, 'candidate_profiles/no_profile.html')  # You'll need to create this template
+        return render(request, 'candidate_profiles/no_profile.html')
 
     context = {'candidate': candidate}
     return render(request, 'candidate_profiles/candidate_profile.html', context)
+
+
+@login_required
+def edit_profile(request):
+    pass
 
 
 @login_required
@@ -23,11 +27,11 @@ def delete_profile_and_account(request):
         candidate = request.user.candidate_profile
     except Candidate.DoesNotExist:
         messages.error(request, "Candidate profile not found.")
-        return redirect('home')  # Or some other appropriate page
+        return redirect('home')
 
     if request.method == 'POST':
         user = request.user
-        logout(request)  # Log the user out before deleting
+        logout(request)
         user.delete()
         messages.success(request, "Your profile and account have been deleted successfully.")
         return redirect('home')  # Redirect to the homepage

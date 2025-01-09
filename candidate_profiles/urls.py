77b507bdac3b85aa059c5845aa8025580bda8530
tibssy <norbert.tiborcz@gmail.com>
2025-01-09 14:08:33 +0000

    Create candidate history view

--- candidate_profiles/urls.py
+++ candidate_profiles/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('profile/', views.candidate_profile, name='candidate_profile'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/history/', views.candidate_history, name='candidate_history'),
     path('delete_profile/', views.delete_profile_and_account, name='delete_profile_and_account'),
 ]
--- candidate_profiles/views.py
+++ candidate_profiles/views.py
@@ -12,9 +12,19 @@ def candidate_profile(request):
     except Candidate.DoesNotExist:
         return render(request, 'candidate_profiles/no_profile.html')
 
+    context = {'candidate': candidate}
+    return render(request, 'candidate_profiles/candidate_profile.html', context)
+
+@login_required
+def candidate_history(request):
+    try:
+        candidate = request.user.candidate_profile
+    except Candidate.DoesNotExist:
+        return render(request, 'candidate_profiles/no_profile.html')  # Or handle appropriately
+
     interviews = candidate.interviews.order_by('-interview_date')
     context = {'candidate': candidate, 'interviews': interviews}
-    return render(request, 'candidate_profiles/candidate_profile.html', context)
+    return render(request, 'candidate_profiles/candidate_history.html', context)
 
 @login_required
 def edit_profile(request):

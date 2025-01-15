0eef0982f60ff5d971d6842eb527bbfad42e776c
tibssy <norbert.tiborcz@gmail.com>
2025-01-15 20:29:27 +0000

    Add newest jobs section to home

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from jobposts.models import JobPost
+
+# def home(request):
+#     return render(request, 'home/index.html')
 
 def home(request):
-    return render(request, 'home/index.html')
+    # Fetch the three newest job posts
+    newest_jobs = JobPost.objects.order_by('-created_on')[:3]
+    return render(request, 'home/index.html', {'newest_jobs': newest_jobs})

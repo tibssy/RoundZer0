3d2f55da95f5acab4f2e6f1180f294459ecfa297
tibssy <norbert.tiborcz@gmail.com>
2025-01-21 15:03:16 +0000

    Add icon to preview my job

--- employer_profiles/urls.py
+++ employer_profiles/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 from candidate_profiles import views as candidate_views
+from jobposts import views as jobpost_views
+
 
 urlpatterns = [
     path('profile/', views.employer_profile, name='employer_profile'),
@@ -8,6 +10,7 @@ urlpatterns = [
     path('delete_profile/', views.delete_employer_profile, name='delete_employer_profile'),
     path('my_jobs/', views.my_jobs, name='employer_jobs'),
     path('my_jobs/create/', views.create_job, name='create_job'),
+    path('my_jobs/view/<int:pk>/', jobpost_views.JobDetailView.as_view(), name='view_my_job'),
     path('my_jobs/edit/<int:job_id>/', views.edit_my_jobs, name='edit_my_jobs'),
     path('my_jobs/delete/<int:job_id>/', views.delete_my_job, name='delete_my_job'),
     path('my_jobs/applications/<int:job_id>/', views.job_applications, name='job_applications'),
